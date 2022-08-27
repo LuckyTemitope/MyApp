8b93f8d1ca2c02a3fcca90ffc7ef8224b4465764
Lucky Temitope <osunbiyi.temitope1@gmail.com>
2022-08-27 16:34:17 -0400

    Added the books component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BooksComponent } from './books/books.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+@NgModule( {
+
+  declarations: [AppComponent, BooksComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
-})
-export class AppModule { }
+}
+)
+
+export class AppModule {}
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from "../Models/books.interface";
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.scss']
+})
+export class BooksComponent implements OnInit {
+
+
+books : Book[] = [
+    {
+      name : "Clean Code",
+      author : "Robert C. Martin",
+      imgUrl : "https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg",
+      disabled: false
+    },
+    {
+      name: "Pragmatic Programmer",
+      author: "David Thomas",
+      imgUrl: "https://images-na.ssl-images-amazon.com/images/I/51W1sBPO7tL._SX380_BO1,204,203,200_.jpg",
+      disabled: true
+    },
+    {
+      name: "Advanced Python Programming",
+      author: "Quan Nguyen",
+      imgUrl: "https://images-na.ssl-images-amazon.com/images/I/410WdyT2qpL._SX404_BO1,204,203,200_.jpg",
+      disabled: false
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    alert("Book Saved")
+  }
+
+}
